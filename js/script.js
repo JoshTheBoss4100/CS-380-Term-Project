45eb66c75ec0b309a8291347856be445565ef49d
jordan white <jordanwhite0924@gmail.com>
2025-04-16 09:08:46 -0400

    git commit -m

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@ const bookData = [{
         book: {
             author:"Lynette Noni",
              title: "Arkanae",
-             price: "12.99",
              genre: "Fantasy",
-            image:{
-                hardcover: "bookImages/arkanaehc.jpg",
-                softcover: "bookImages/arkanaesc.jpg"
+            choice:{
+                softcover:{ 
+                    image: "bookImages/arkanaehc.jpg",
+                    price: "12.99"
+                },
+                hardcover: {
+                    image: "bookImages/arkanaesc.jpg",
+                    price: "18.99"
+                }
             }
         },
         demand: 1,
@@ -16,16 +21,242 @@ const bookData = [{
 {
     book: {
         author:"Lynette Noni",
-         title: "Arkanae",
-         price: "12.99",
+         title: "Raelia",
          genre: "Fantasy",
-        image:{
-            hardcover: "bookImages/arkanaehc.jpg",
-            softcover: "bookImages/arkanaesc.jpg"
+         choice:{
+            softcover:{ 
+                image: "bookImages/Raeliasc.jpg",
+                price: "12.99"
+            },
+            hardcover: {
+                image: "bookImages/raeliahc.jpeg",
+                price: "18.99"
+            }
         }
     },
     demand: 1,
     favorite: true,
     bestseller: false
 },
+{
+    book: {
+        author:"Lynette Noni",
+         title: "Draekora",
+         genre: "Fantasy",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Draekorasc.jpg",
+                price: "12.99"
+            },
+            hardcover: {
+                image: "bookImages/Draekorasc.jpg",
+                price: "18.99"
+            }
+        }
+    },
+    demand: 1,
+    favorite: true,
+    bestseller: false
+},
+{
+    book: {
+        author:"Lynette Noni",
+         title: "Graevale",
+         genre: "Fantasy",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Graevalesc.jpg",
+                price: "12.99"
+            },
+            hardcover: {
+                image: "bookImages/Graevalesc.jpg",
+                price: "18.99"
+            }
+        }
+    },
+    demand: 1,
+    favorite: true,
+    bestseller: false
+},
+{
+    book: {
+        author:"Lynette Noni",
+         title: "Vardaesia",
+         genre: "Fantasy",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Vardaesiasc.jpg",
+                price: "12.99"
+            },
+            hardcover: {
+                image: "bookImages/Vardaesiasc.jpg",
+                price: "18.99"
+            }
+        }
+    },
+    demand: 1,
+    favorite: true,
+    bestseller: false
+},
+{
+    book: {
+        author:"Rebecca Yarros",
+         title: "Fourth Wing",
+         genre: "Fantasy",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Fourthwingsc.jpeg",
+                price: "15.99"
+            },
+            hardcover: {
+                image: "bookImages/Fourthwinghc.jpg",
+                price: "19.99"
+            }
+        }
+    },
+    demand: 5,
+    favorite: true,
+    bestseller: true
+},
+{
+    book: {
+        author:"Rebecca Yarros",
+         title: "Iron Flame",
+         genre: "Fantasy",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Ironflamesc.jpg",
+                price: "15.99"
+            },
+            hardcover: {
+                image: "bookImages/Ironflamehc.jpg",
+                price: "19.99"
+            }
+        }
+    },
+    demand: 5,
+    favorite: false,
+    bestseller: true
+},
+{
+    book: {
+        author:"Rebecca Yarros",
+         title: "Onyx Storm",
+         genre: "Fantasy",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Onyxstormsc.jpg",
+                price: "18.99"
+            },
+            hardcover: {
+                image: "bookImages/Onyxstormhc.jpg",
+                price: "24.99"
+            }
+        }
+    },
+    demand: 5,
+    favorite: false,
+    bestseller: true
+},
+{
+    book: {
+        author:"Markus Zusak",
+         title: "The Book Theif",
+         genre: "Historical Fiction",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Booktheifsc.jpg",
+                price: "11.99"
+            },
+            hardcover: {
+                image: "bookImages/Booktheifsc.jpg",
+                price: "15.99"
+            }
+        }
+    },
+    demand: 1,
+    favorite: true,
+    bestseller: false
+},
+{
+    book: {
+        author:"Kristin Hannah",
+         title: "The Nightingale",
+         genre: "Historical Fiction",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Nightingalesc.jpg",
+                price: "16.99"
+            },
+            hardcover: {
+                image: "bookImages/Nightingalehc.jpg",
+                price: "22.99"
+            }
+        }
+    },
+    demand: 1,
+    favorite: false,
+    bestseller: false
+},
+{
+    book: {
+        author:"Anthony Doerr",
+         title: "All The Light We Cannot See",
+         genre: "Historical Fiction",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Lightcannotseesc.jpg",
+                price: "15.99"
+            },
+            hardcover: {
+                image: "bookImages/Lightcannotseehc.jpeg",
+                price: "19.99"
+            }
+        }
+    },
+    demand: 1,
+    favorite: false,
+    bestseller: false
+},
+{
+    book: {
+        author:"Madeline Miller",
+         title: "The Song of Achilles",
+         genre: "Historical Fiction",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Archillessc.jpg",
+                price: "12.99"
+            },
+            hardcover: {
+                image: "bookImages/Achilleshc.jpeg",
+                price: "18.99"
+            }
+        }
+    },
+    demand: 1,
+    favorite: false,
+    bestseller: true
+},
+{
+    book: {
+        author:"Colleen Hoover",
+         title: "It Ends With Us",
+         genre: "Romance",
+         choice:{
+            softcover:{ 
+                image: "bookImages/Itendswithussc.jpg",
+                price: "14.99"
+            },
+            hardcover: {
+                image: "bookImages/Itendswithussc.jpg",
+                price: "21.99"
+            }
+        }
+    },
+    demand: 5,
+    favorite: false,
+    bestseller: true
+},
+
 ]
