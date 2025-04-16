0c316c8e9c3356a893e6c9df8e4e453fa9950235
jordan white <jordanwhite0924@gmail.com>
2025-04-16 08:26:29 -0400

    folder

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const bookData = [{
+        book: {
+            author:"Lynette Noni",
+             title: "Arkanae",
+             price: "12.99",
+             genre: "Fantasy",
+            image:{
+                hardcover: "bookImages/arkanaehc.jpg",
+                softcover: "bookImages/arkanaesc.jpg"
+            }
+        },
+        demand: 1,
+        favorite: true,
+        bestseller: false
+    },
+{
+    book: {
+        author:"Lynette Noni",
+         title: "Arkanae",
+         price: "12.99",
+         genre: "Fantasy",
+        image:{
+            hardcover: "bookImages/arkanaehc.jpg",
+            softcover: "bookImages/arkanaesc.jpg"
+        }
+    },
+    demand: 1,
+    favorite: true,
+    bestseller: false
+},
+]
