0074bb5042cbd0d5ee50541097da2754456f326b
JoshTheBoss4100 <Joshtheboss4100@outlook.com>
2025-04-20 12:11:30 -1100

    Finished specific book page.

--- js/function.js
+++ js/function.js
@@ -1,3 +1,8 @@
+if (localStorage.getItem('bookData')) {
+    bookData = JSON.parse(localStorage.getItem('bookData'));
+}
+
+
 function DisplayBestSellers(){
     let count = 0;
     for (book in bookData)
@@ -51,16 +56,62 @@ window.addEventListener("load", function(){
             });
     }
 });
-
-
-function BookInfo(){
+function grabBookId(){
     let urlParams = new URLSearchParams(window.location.search);
     let index = urlParams.get('id');
+    return index;
+}
+
+function BookInfo(){
+    let index = grabBookId();
     let bookHTML = `
      <figure>
     <img src="${bookData[index].book.choice.softcover.image}" alt="${bookData[index].book.title}">
     </figure>`;
     document.getElementById("book").innerHTML += bookHTML;
-    document.getElementById("synopsis").innerHTML += bookData[index].book.description;
-   /* document.getElementById("add_cart").innerHTML += 1; */
+    document.getElementById("synopsis").innerHTML += `<p>${bookData[index].book.description}</p>`;
+    /*document.getElementById("soft_price").innerHTML += bookData[index].book.choice.softcover.price;
+    document.getElementById("hard_price").innerHTML += bookData[index].book.choice.hardcover.price;*/
+    document.getElementById("author").innerHTML += bookData[index].book.author;
+    document.getElementById("title").innerHTML += bookData[index].book.title;
+    document.getElementById("genre").innerHTML += bookData[index].book.genre;
+    document.getElementById("add_to_cart").innerHTML = "";
+    if(bookData[index].inCart==true)
+        document.getElementById("add_to_cart").innerHTML += "Remove from Cart";
+    else
+        document.getElementById("add_to_cart").innerHTML += "Add to Cart";
+
 }  
+
+function addToCart(){
+    document.getElementById("add_to_cart").addEventListener("click", function(){
+    let index = grabBookId();
+    bookData[index].inCart = !bookData[index].inCart;
+    document.getElementById("add_to_cart").innerHTML = "";
+    if(bookData[index].inCart==true)
+        document.getElementById("add_to_cart").innerHTML += "Remove from Cart";
+    else
+        document.getElementById("add_to_cart").innerHTML += "Add to Cart";
+    
+    localStorage.setItem('bookData', JSON.stringify(bookData));
+
+   
+
+
+});
+}
+
+window.addEventListener("load", function(){
+    if (document.getElementById("soft") && document.getElementById("hard")){
+    document.getElementById("soft").addEventListener("change", updatePrice);
+    document.getElementById("hard").addEventListener("change", updatePrice);
+    }
+})
+
+function updatePrice(){
+    let index = grabBookId();
+    if (document.getElementById("soft").checked)
+        document.getElementById("price").textContent = "Price: $" + bookData[index].book.choice.softcover.price;
+    else if (document.getElementById("hard").checked)
+        document.getElementById("price").textContent = "Price: $" + bookData[index].book.choice.hardcover.price;
+}
